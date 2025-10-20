c047bb09ad954caded3bfb6f1f2b3b4f966cf9b1
Mark Khoury <mark@markkhoury.me>
2025-10-20 21:36:27 +0000

    Refactored cln_card_transactions w/ best practices of sources.yml and macros

--- models/cln_card_transactions.sql
+++ models/cln_card_transactions.sql
@@ -1,14 +1,39 @@
+-- Import file and cast
+-- Apply lowercase `description` field to improve downstream query performance
+with src as (
+    select
+        cast(key as string)                     as key
+        , cast(description as string)           as description
+        , cast(date as date)                    as date
+        , cast(type as string)                  as type_raw
+        , cast(category as string)              as string
+        , safe_cast(amount as numeric)          as amount
+        , safe_cast(card_last4 as int64)        as card_last4
+        , safe_cast(counter as numeric)         as counter
+        , safe_cast(intermediate_key as string) as intermediate_key
+        , lower(cast(description as string))    as description_lower
+    from
+        {{source('warehouse', 'card_transactions')}}
+    where
+        1=1
+)
+
+, classified_transactions as (
+    select
+        *
+        , case
+            when s.type_raw is not null then s.type_raw
+            when s.card_last4 not in ({{ var('payment_card_last4_list', [3221,4245,5083,6823]) | join(',') }})
+                then 'Payment'
+            when {{ payments_type_keyword_match('s.description') }} 
+                then 'Payment'
+            else 'Sale'
+        end as type
+    from
+        src as s
+)
+
 select
-    cast(key as string)                     as key
-    , cast(description as string)           as description
-    , cast(date as date)                    as date
-    , cast(type as string)                  as type_raw
-    , cast(category as string)              as string
-    , safe_cast(amount as numeric)          as amount
-    , safe_cast(card_last4 as int64)        as card_last4
-    , safe_cast(counter as numeric)         as counter
-    , safe_cast(intermediate_key as string) as intermediate_key
+    *
 from
-    {{source('warehouse', 'card_transactions')}}
-where
-    1=1
+    classified_transactions;
