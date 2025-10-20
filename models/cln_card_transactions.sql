4628530073979a9d900fe7f6dd230c478a11b8f4
Mark Khoury <mark@markkhoury.me>
2025-10-20 21:35:11 +0000

    Refactor cln_card_transactions following best practices for sources.yml and macro structure

--- models/cln_card_transactions.sql
+++ models/cln_card_transactions.sql
@@ -1,17 +1,14 @@
 select
-    ct.key
-    , ct.description
-    , ct.date
-    , ct.type
-    , ct.amount
-    , ct.category
-    , ct.card_last4
-    , ct.counter
-    , ct.number_instances
-    , ct.intermediate_key 
+    cast(key as string)                     as key
+    , cast(description as string)           as description
+    , cast(date as date)                    as date
+    , cast(type as string)                  as type_raw
+    , cast(category as string)              as string
+    , safe_cast(amount as numeric)          as amount
+    , safe_cast(card_last4 as int64)        as card_last4
+    , safe_cast(counter as numeric)         as counter
+    , safe_cast(intermediate_key as string) as intermediate_key
 from
-    warehouse.card_transactions as ct
+    {{source('warehouse', 'card_transactions')}}
 where
     1=1
-order by
-    ct.date desc
