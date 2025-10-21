d108d0bb994c6beef2434759d31d0e6ed9e328a5
Mark Khoury <mark@markkhoury.me>
2025-10-21 17:48:08 +0000

    Updated sources.yml and downstream files

--- models/cln_merchants.sql
+++ models/cln_merchants.sql
@@ -4,10 +4,10 @@
 -- Import file and cast
 with src_merchants as (
     select
-        cast(merchant_key as string)        as merchant_key
-        , cast(merchant_name as string)     as merchant_name
-        , cast(created_at as timestamp)     as created_at
-        , cast(updated_at as timestamp)     as updated_at
+        cast(id as int64)                   as id
+        , cast(name as string)              as merchant_name
+        , cast(merchant_key as string)      as merchant_key
+        , cast(subcategory_id as int64)     as subcategory_id
     from
         {{source('warehouse', 'merchants')}}
 )
