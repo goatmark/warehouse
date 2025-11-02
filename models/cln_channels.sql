55f5811569be44ef60c412a779837a873a28c050
Mark Khoury <mark@markkhoury.me>
2025-11-02 19:24:32 +0000

    Updated cln_channels and cln_videos with website DB

--- models/cln_channels.sql
+++ models/cln_channels.sql
@@ -5,7 +5,7 @@ with src as (
     select
         cast(src.id as string)                  as id_raw
         , cast(src.handle as string)            as handle
-        , cast(src.channel_id as string)        as channel_id
+        , cast(src.channel_id as string)        as id
         , cast(src.created_at as timestamp)     as created_at
         , cast(src.updated_at as timestamp)     as updated_at
     from
@@ -13,8 +13,7 @@ with src as (
 )
 
 select
-    id_raw
-    , id
+    id
     , handle
     , created_at
     , updated_at
