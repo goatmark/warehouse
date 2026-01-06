2a5c03b273b9dab2d78c908b5f090482b3ef237d
Mark Khoury <mark@markkhoury.me>
2026-01-06 03:34:32 +0000

    Updated cln_exercises with data from new exercises model in Supabase

--- models/cln_exercises.sql
+++ models/cln_exercises.sql
@@ -1,8 +1,22 @@
 -- cln_exercises.sql
 {{ config(materialized='view') }}
 
--- Aggregate muscle loads into JSON array per exercise
+-- Raw exercises cast
 with src as (
+    select
+        cast(src.name as string)                        as exercise_name
+        , cast(src.exercise_type as string)             as exercise_type
+        , cast(src.coeff as numeric)                    as coeff
+        , cast(src.load_multiplier_method as string)    as method
+        , cast(src.max_weight__lbs_ as numeric)         as max_weight_lbs  
+    from
+        {{source('warehouse', 'exercises')}} as src
+    where
+        1=1 
+)
+
+-- Aggregate muscle loads into JSON array per exercise
+, src_muscle as (
     select
         cast(exercise_name as string) as exercise_name
         , cast(muscle_name as string) as muscle_name
@@ -26,15 +40,32 @@ with src as (
             order by weight desc
         ) as muscles_array
     from
-        src
+        src_muscle
     where
         1=1
     group by
         exercise_name
 ) 
 
-select
+-- Consolidate
+, final_agg as (
+    select
     exercise_name
     , to_json_string(muscles_array) as muscles
 from 
     aggregated
+
+)
+
+-- Merge from skeleton defined in source
+select
+    src.exercise_name
+    , src.exercise_type
+    , src.coeff
+    , src.method
+    , src.max_weight_lbs  
+    , final_agg.muscles
+from
+    src
+left join final_agg on
+    src.exercise_name = final_agg.exercise_name
